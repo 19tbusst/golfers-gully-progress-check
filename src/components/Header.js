367d733482f0955b11b62658ffef26455dbf9a87
Tom Busst <tombusstnz@gmail.com>
2022-06-10 10:36:26 +1200

    initial

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <a href="/">HOME</a>
+            <a href="/">ABOUT</a>
+            <a href="/">CONTACT</a>
+        </header>
+    );
+};
