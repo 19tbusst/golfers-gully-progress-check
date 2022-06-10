44e85c87a9ec2bb44bac5ba270844ea55c38825d
Tom Busst <tombusstnz@gmail.com>
2022-06-10 12:52:25 +1200

    pain

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 export default function Header() {
     return (
-        <nav className="header">
-            <a href="/">HOME</a>
-            <a href="/">ABOUT</a>
-            <a href="/">CONTACT</a>
-        </nav>
+        <div className="Header">
+            <nav>
+                <a href="/">HOME</a>
+                <a href="/">ABOUT</a>
+                <a href="/">CONTACT</a>
+            </nav>
+        </div>
     );
 };
